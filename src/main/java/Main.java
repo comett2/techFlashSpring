a128883362cbecbe48d39730a95bb058e9b2b056
michal <michal.koman.89@gmail.com>
2016-03-20 20:31:56 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String args[]) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Person person = context.getBean("person", Person.class);
+        System.out.println(person);
+    }
+}
